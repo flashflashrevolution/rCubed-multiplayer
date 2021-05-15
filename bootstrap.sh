f2e4475970a53feb90d83e58a11a8ae476f14cc7
Zageron <hello@adambryant.ca>
2021-05-14 18:11:00 -0700

    Fix bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,2 @@
-wget -O https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
-tar xf OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz -- directory sdk
+wget -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
+tar xf openjdk.tar.gz -- directory sdk
