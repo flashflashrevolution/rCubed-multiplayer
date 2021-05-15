9c009b3ea517744009b6d3c5c5d69cdc6e978932
Zageron <hello@adambryant.ca>
2021-05-15 01:21:45 +0000

    Update bootstrap.sh to have progress and proper extraction.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
-wget -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
-tar xf openjdk.tar.gz -- directory sdk
+wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
+mkdir -p jdk
+pv openjdk.tar.gz | tar --strip-components=1 --no-overwrite-dir -zxf - -C ./jdk
+rm -f openjdk.tar.gz
