daa7251fc747b439c4914527305b6ef20082e953
Zageron <hello@adambryant.ca>
2021-05-15 09:33:03 -0700

    Finalized addition of smartfox to bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,43 @@
+# Reset
+echo "\nReset"
+rm -rf sfs
+rm -rf jdk
+
+# Install JDK 1.8
+echo "\nInstall JDK 1.8"
 wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz
 mkdir -p jdk
 pv openjdk.tar.gz | tar --strip-components=1 --no-overwrite-dir -zxf - -C ./jdk
-rm -f openjdk.tar.gz
 
 # Install SmartFoxServer 1.6.6
+echo "\nInstall SmartFoxServer 1.6.6"
 wget -nc -O sfs-1.6.6.tar.gz https://www.smartfoxserver.com/downloads/sfs1/SFSPRO_linux64_1.6.6.tar.gz
 mkdir -p sfs-temp
 pv sfs-1.6.6.tar.gz | tar --strip-components=1 --no-overwrite-dir -zxf - -C ./sfs-temp
-printf "$PWD/sfs" | ./sfs-temp/install
-rm -rf sfs-temp
+cd sfs-temp
+printf "$(dirname "$(cd -P -- "$(dirname -- "$0")" && pwd -P)")"/sfs | ./install
 
 # Install SmartFoxServer 1.6.20 Patch
+echo "\nInstall SmartFoxServer 1.6.20 Patch"
+cd ..
 wget -nc -O sfs-patch-1.6.20.zip https://www.smartfoxserver.com/downloads/sfs1/SFSPRO_Patch_1.6.20.zip
-unzip -o sfs-patch-1.6.20.zip -d sfs-patch | pv -l >/dev/null
-cp -R sfs-patch/SFSPRO_Patch_1.6.20/Server/ sfs/SFS_PRO_1.6.6/Server/lib
+unzip -o sfs-patch-1.6.20.zip -d sfs-patch
+cp --verbose -rf sfs-patch/SFSPRO_Patch_1.6.20/Server/ sfs/SFS_PRO_1.6.6/Server/lib
+
+echo "\nRemove Build Artifacts"
+rm -f openjdk.tar.gz
+rm -f sfs-1.6.6.tar.gz
+rm -f sfs-patch-1.6.20.zip
+rm -rf sfs-temp
 rm -rf sfs-patch
 
-
 # Git Configuration
+echo "\nGit Configuration"
 git config --local core.fileMode false
 git lfs pull
 git config --local core.editor "code --wait"
+
+
+# Config Configuration
+echo "\nConfig ln Configuration"
+ln -sfv /workspaces/rCubed-multiplayer/config /workspaces/rCubed-multiplayer/sfs/SFS_PRO_1.6.6/Server/
--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-ln -sfv /workspaces/rCubed-multiplayer/config /workspaces/rCubed-multiplayer/sfs/SFS_PRO_1.6.6/Server/
 /workspaces/rCubed-multiplayer/sfs/SFS_PRO_1.6.6/Server/wrapper -c /workspaces/rCubed-multiplayer/config/wrapper.develop.conf
