5a60767e7576ad554fd6f3c313a39f76302672c1
Zageron <hello@adambryant.ca>
2021-05-16 17:17:30 -0700

    Update start.sh to start SFS2X.

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/workspaces/rCubed-multiplayer/sfs/SFS_PRO_1.6.6/Server/wrapper -c /workspaces/rCubed-multiplayer/config/wrapper.develop.conf
+cd /workspaces/rCubed-multiplayer/sfs/SFS2X/
+./sfs2x.sh
