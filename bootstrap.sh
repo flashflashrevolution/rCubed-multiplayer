243f2988a42dbb8388a68e549c0c0b0621617124
Zageron <hello@adambryant.ca>
2021-05-16 17:24:17 -0700

    Upgrade to JDK16 and link to SFS2X.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,8 @@ if git diff --quiet HEAD -- ':!bootstrap.sh'; then
     echo "\nInstall JDK"
     #wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_linux_hotspot_8u292b10.tar.gz # JDK 1.8
     #wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.11%2B9/OpenJDK11U-jdk_x64_linux_hotspot_11.0.11_9.tar.gz # JDK 11
-    wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_linux_hotspot_15.0.2_7.tar.gz # JDK 15
+    #wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_linux_hotspot_15.0.2_7.tar.gz # JDK 15
+    wget -nc -O openjdk.tar.gz https://github.com/AdoptOpenJDK/openjdk16-binaries/releases/download/jdk-16.0.1%2B9/OpenJDK16U-jdk_x64_linux_hotspot_16.0.1_9.tar.gz # JDK 16
     mkdir -p jdk
     pv openjdk.tar.gz | tar --strip-components=1 --no-overwrite-dir -zxf - -C ./jdk
 
@@ -16,6 +17,11 @@ if git diff --quiet HEAD -- ':!bootstrap.sh'; then
     wget -nc -O sfs2x-2.17.0.tar.gz https://www.smartfoxserver.com/downloads/sfs2x/SFS2X_unix_2_17_0.tar.gz
     mkdir -p sfs
     pv sfs2x-2.17.0.tar.gz | tar --strip-components=1 --no-overwrite-dir -zxf - -C ./sfs
+
+    # Replace the embedded SmartFox JDK with our downloaded one.
+    echo "\nReplace embedded JDK with downloaded one."
+    rm -rf sfs/jre
+    ln -sfv /workspaces/rCubed-multiplayer/jdk /workspaces/rCubed-multiplayer/sfs/jre
 
     echo "\nRemove Build Artifacts"
     rm -f openjdk.tar.gz
